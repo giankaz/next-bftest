241c5e75705cc8f0358626d4314b25e7460cbd67
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-22 21:18:13 -0300

    fix: site name

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
 
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import Head from 'next/head'
 import LinearIndeterminate from '../components/UI/LinearMUI'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const router = useRouter()
 
-  useEffect(()=> {
+  useEffect(() => {
     router.push('/login')
-  },[])
+  }, [])
 
   return (
-    <div className={styles.container}>
-      <LinearIndeterminate/>
-    </div>
+    <>
+      <Head>
+        <title>Disparo PRO</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={styles.container}>
+        <LinearIndeterminate />
+      </div>
+    </>
   )
 }
